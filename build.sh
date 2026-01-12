a546bc44b55939d12b3e0ee26c51e08fa6d2d6c3
KunalDuran <kunalduran13@gmail.com>
2026-01-13 02:12:31 +0530

    mod file and build script win cmd

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 GOARCH=arm GOARM=7 GOOS=linux go build -o bin/armv7l ./cmd/client 
-GOARCH=arm64 GOARM="" GOOS=linux go build -o bin/aarch64 ./cmd/client 
+GOARCH=arm64 GOARM="" GOOS=linux go build -o bin/aarch64 ./cmd/client
+
+# $env:GOARCH = "arm"; $env:GOARM = "7"; $env:GOOS = "linux"; go build -o bin/armv7l ./cmd/client
+# $env:GOARCH = "arm64"; $env:GOARM = ""; $env:GOOS = "linux"; go build -o bin/dronnayakc ./cmd/client
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/KunalDuran/dronnayak
+module github.com/KunalDuran/dronnayak-core
 
 go 1.24.0
 
